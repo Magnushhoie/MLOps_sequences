bcb49c401be3c34af1b02b841877b534b1e26fe4
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-01-07 12:45:59 +0100

    :tada: Use Cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DTU MLOps Project GitHub',
+    author='Magnus Høie, Ricardo Hernández Medina, Alessandro Montemurro',
+    license='MIT',
+)
